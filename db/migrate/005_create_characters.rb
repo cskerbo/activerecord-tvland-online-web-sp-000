1ba721460073158b667a4d6c46cf99b8e6fe7b17
Chris Kerbo <chris.kerbo@outlook.com>
2019-11-03 22:37:51 +0000

    Automatically backed up by Learn

--- db/migrate/005_create_characters.rb
+++ db/migrate/005_create_characters.rb
@@ -4,6 +4,6 @@ class CreateCharacters < ActiveRecord::Migration[5.1]
     create_table :characters do |x|
       x.string :name
       x.integer :actor_id
-      x.integer : show_id
+      x.integer :show_id
     end
   end

d5aa4403b7bfe558ee293181a6d4a0c3091a0d01
Chris Kerbo <chris.kerbo@outlook.com>
2019-11-03 23:13:27 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -4,7 +4,7 @@ class Show < ActiveRecord::Base
   belongs_to :network
 
   def actors_list
-    actors.collect do |actor|
+    characters.collect do |character|
       "#{actor.name}"
     end
   end
